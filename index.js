9bdd7f6741f0e8ddea8d67fb9fcfb7bd64b2fffd
caroline-ctrl <duon.caroline@gmail.com>
2020-03-24 12:06:25 +0100

    create generalMethod

---
+++ index.js
@@ -0,0 +1,8 @@
+// pas besoin de charger le module fs car je ne veux pas l'enregistrer dans un fichier mais le faire apparaitre
+// sur la page internet.
+
+const generalMethod = (req, res) => {
+    const url = req.url;
+    const model = req.model;
+
+}
